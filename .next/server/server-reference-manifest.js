efeed53b06605bc62516e7dbe215d6055493848e
Saiful Shaik <Saifulshaik47@gmail.com>
2025-07-17 08:24:33 -0400

    Add GitHub Pages deployment workflow and fix configuration

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3mismyrVIQlVozMYm6M0SPHEnVNA7QFI66Od95jCAmI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ceSvtIq+soZaoJo7dF7RdVmn6KpZuyeClvTuWRAVmRA=\"\n}"
